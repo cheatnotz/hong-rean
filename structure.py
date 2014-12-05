1d7c5c4acfeb201ef1b0f754429cde44a2fb47cf
Kittituch Pavaranchanakul <banzkung@office365.it.kmitl.ac.th>
2014-12-05 10:20:09 +0700

    Edit and complete limited to width's screen

--- structure.py
+++ structure.py
@@ -12,31 +12,36 @@ class App:
         
         self.num_of_stu = IntVar() #input number of student(int value)
 
-        Label(self.top, text="Number of Student :").grid(padx=10, pady=10, row=0, column=0)
-        Entry(self.top, textvariable=self.num_of_stu).grid(padx=10, pady=10, row=0, column=1) #Empty for input
+        Label(self.top, text="Number of \nStudent :").grid(padx=5, pady=5, row=0, column=0)
+        Entry(self.top, textvariable=self.num_of_stu, width=5).grid(padx=5, pady=5, row=0, column=1) #Empty for input
 
-        Button(self.top, text="Submit", command=self.slot).grid(padx=10, pady=10, row=0, column=2)
+        Button(self.top, text="Submit", command=self.slot).grid(padx=5, pady=5, row=0, column=2)
 
     def slot(self):
         self.num = []
         self.grade = []
         Label(self.top, text=" ").grid(padx=10, pady=10, row=1, column=0)
-        Label(self.top, text="Student No.", width=10).grid(padx=10, pady=10, row=1, column=1)
-        Label(self.top, text="Grade", width=10).grid(padx=10, pady=10, row=1, column=2)
+        Label(self.top, text="Student \nNo.", width=5).grid(padx=10, pady=10, row=1, column=1)
+        proceed = Button(self.top, text="Proceed", command=self.selection)
+        Label(self.top, text="Grade", width=5).grid(padx=10, pady=10, row=1, column=2)
         for i in xrange(self.num_of_stu.get()):
             self.num.append("No" + str(i))
             self.grade.append("Grade" + str(i))
             
         for j in xrange(len(self.num)):
-            scrollbar = Scrollbar(self.top)
             self.num[j] = IntVar()
             self.grade[j] = IntVar()
 
-            Label(self.top, text=j+1, width=5).grid(padx=10, pady=10, row=j+2, column=0)
-            Entry(self.top, textvariable=self.num[j], width=10).grid(padx=10, pady=10, row=j+2, column=1)
-            Entry(self.top, textvariable=self.grade[j], width=10).grid(padx=10, pady=10, row=j+2, column=2)
-        proceed = Button(self.top, text="Proceed", command=self.selection)
-        proceed.grid(row=len(self.num)+2, column=2, padx=10, pady=10)
+            if j > 9:
+                Label(self.top, text=j+1, width=5).grid(padx=10, pady=5, row=2+j%10, column=2+(3*(j/10)-2))
+                Entry(self.top, textvariable=self.num[j], width=5).grid(padx=10, pady=5, row=2+j%10, column=3+(3*(j/10)-2))
+                Entry(self.top, textvariable=self.grade[j], width=5).grid(padx=10, pady=5, row=2+j%10, column=4+(3*(j/10)-2))
+                proceed.grid(row=len(self.num)+2, column=4+(3*(j/10)-2), padx=20, pady=15)
+            else:
+                Label(self.top, text=j+1, width=5).grid(padx=10, pady=5, row=2+j, column=0)
+                Entry(self.top, textvariable=self.num[j], width=5).grid(padx=10, pady=5, row=2+j, column=1)
+                Entry(self.top, textvariable=self.grade[j], width=5).grid(padx=10, pady=5, row=2+j, column=2)
+                proceed.grid(row=len(self.num)+2, column=2, padx=20, pady=15)
         
     def selection(self):
         self.topend = Toplevel()
